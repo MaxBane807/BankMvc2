5df4c69cd02ae2ce659f6b31fba832e886218174
MaxBane807 <55383373+MaxBane807@users.noreply.github.com>
2020-04-25 13:37:23 +0200

    Fixed Migrations

---
+++ Bank/Migrations/20200425112524_FirstTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bank.Migrations
+{
+    public partial class FirstTest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TestColumn",
+                table: "Customers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TestColumn",
+                table: "Customers");
+        }
+    }
+}
